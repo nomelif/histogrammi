757db28e4bc05eae02e26dfd2fd399aeb3171c28
Théo Friberg <theo.friberg@gmail.com>
2015-02-12 10:41:12 +0200

    Cleaned up the script.js-file

--- script.js
+++ script.js
@@ -215,80 +215,146 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
            
         }
 
+        // Calculate the exact values of the multipliers
+
         kerroin_x = 520. / sum_x;
         kerroin_y = 325. / sum_y;
 
-        var width = 0;
+        // List of colors to use
+
         var colors = ["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548"];
+        
+        // Pick a first color at random
+
         var c = Math.floor(Math.random()*colors.length);
 
-        for(var i = 0; i < vals.length; i++){
-           //do something to each div like
+        // Iterate over both colors and bars
 
-           width += tick(width, ctx, vals[i][1]*kerroin_y, vals[i][0]*kerroin_x, colors[c%colors.length]);
-           c++;
+        for(var i = 0; i < vals.length; i++){
+        
+            // Draw the bar
+
+            tick(width, ctx, vals[i][1]*kerroin_y, vals[i][0]*kerroin_x, colors[c%colors.length]);
+            
+            // Increment the iterator for the colors
+
+            c++;
            
 
         }
+
+        // Draw the axes
+
         axis(ctx);
+
+        // Prepare fonts for the text
 
         ctx.font = "16px Roboto";
         ctx.fillStyle = "black";
+        ctx.textAlign = "center";
          
-        // write the text
+        // Calculate the order of magnitude for the plot's x-axis
+
         var i = 1;
         while(Math.pow(10, i) < sum_x){
             i = i + 1;
         }
-        ctx.textAlign = "center";
+
+        // Draw the adequate text to the adequate place
+
         ctx.fillText( String(Math.pow(10, i-1)), 370-Math.pow(10, i-1)*kerroin_x, 352);
+        
+        // Draw the line indicating scale of the x-axis
+
         ctx.moveTo(370-Math.pow(10, i-1)*kerroin_x, 355);
         ctx.lineTo(370-Math.pow(10, i-1)*kerroin_x, 375);
+
+        // Calculate the order of magnitude for the plot's y-axis
+
         i = 1;
         while(Math.pow(10, i) < sum_y){
             i = i + 1;
         }
+
+        // Change the alignment of the text & draw the text
+
         ctx.textAlign = "start";
         ctx.fillText( String(Math.pow(10, i-1)), 41, 370-Math.pow(10, i-1)*kerroin_y);
+
+        // Draw the line indicating the scale of the y-axis
 
         ctx.save();
         ctx.moveTo(20, 364-Math.pow(10, i-1)*kerroin_y);
         ctx.lineTo(40, 364-Math.pow(10, i-1)*kerroin_y);
-        ctx.stroke();
-        ctx.restore();
 
-        ctx.save();
+        // Actually draw the pahts
+
         ctx.stroke();
+
+        // Restore the context's status & save it again
+
         ctx.restore();
+        ctx.save();
+
+        // Draw the horizontal bars onto the plot
+
         hbars(ctx, kerroin_y, i);
     }
 
-    function vbars(ctx, scale, i){
-        
-    }
+    // Function to draw the horizontal bars of the plot
 
     function hbars(ctx, scale, i){
+
+        // Save the context
+
         ctx.save();
+
+        // Start drawing (important)
+
         ctx.beginPath();
+
+        // Set the color of the path
+
         ctx.strokeStyle = "#626262";
+
+        // Iterate over the bars, if the scale is small use more, if it's big, use less
+
         var ii = 1;
         if(i < 2){
+
+            // If we have a small scale, use nineteen bars
+
             while(ii < 20){
                 ctx.moveTo(25, 364-Math.pow(10, i-1)*scale*ii/2);
                 ctx.lineTo(520, 364-Math.pow(10, i-1)*scale*ii/2);
                 ii = ii + 1;
             }
         }else{
+
+            // Else use nine bars 
+
             while(ii < 10){
                 ctx.moveTo(25, 364-Math.pow(10, i-1)*scale*ii);
                 ctx.lineTo(520, 364-Math.pow(10, i-1)*scale*ii);
                 ii = ii + 1;
             }
         }
-        //alert(scale);
+
+        // Stroke the path
+
         ctx.stroke();
+
+        // Restore the context
+
         ctx.restore();
     }
+
+    // Function to draw a bar of the plot, the args mean:
+    // -before : how many pixels (of width) are used by previous bars
+    // - ctx   : context to use for drawing
+    // - height: height (in pixels) of the bar
+    // - width : width (in pixels) of the bar
+    // - color : color of the bar
 
     function tick(before, ctx, height, width, color){
         ctx.fillStyle = color;
@@ -296,46 +362,50 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         return width;
     }
 
-    function updateAxis(){
+    // Function used to re-draw the axes and drawing the names / units seen
+    // (This internally rotates the context)
+
+    function updateAxis(ctx){
+
+    // Get the divs containing the names of the units
 
     var left = document.getElementById("vasen");
     var right = document.getElementById("oikea");
 
+    // Get the names of the units
+
     left = left.options[left.selectedIndex].text;
     right = right.options[right.selectedIndex].text;
 
-            var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
+    // Clear the plot
+
     ctx.clearRect(0, 0, c.width, c.height)
-    // start by saving the current context (current orientation, origin)
 
-ctx.save();
- 
-// when we rotate we will be pinching the
-// top-left hand corner with our thumb and finger
-ctx.translate( 0, 0 );
- 
-// now rotate the canvas anti-clockwise by 90 degrees
-// holding onto the translate point
-ctx.rotate( Math.PI / 2 );
- 
-// specify the font and colour of the text
-ctx.font = "16px Roboto";
-ctx.fillStyle = "black"; // red
- 
-// set alignment of text at writing point (left-align)
-ctx.textAlign = "left";
- 
-// write the text
-ctx.fillText( right, 170, -10 );
- 
-// now restore the canvas flipping it back to its original orientation
-ctx.restore();
-ctx.font = "16px Roboto";
-ctx.fillStyle = "black";
- 
-// write the text
-ctx.fillText( left, 260, 385 );
-    }
+    // Start by saving the current context (current orientation, origin)
 
-    updateAxis();
+    ctx.save();
+ 
+    // Now rotate the canvas anti-clockwise by 90 degrees
+
+    ctx.rotate( Math.PI / 2 );
+ 
+    // Specify the font and colour of the text
+    ctx.font = "16px Roboto";
+    ctx.fillStyle = "black"; // red
+     
+    // Set alignment of text at writing point (left-align)
+    ctx.textAlign = "left";
+     
+    // Write the text on the right
+    ctx.fillText( right, 170, -10 );
+     
+    // Now restore the canvas flipping it back to its original orientation
+    ctx.restore();
+    ctx.font = "16px Roboto";
+    ctx.fillStyle = "black";
+     
+    // Write the text on the lefy
+    ctx.fillText( left, 260, 385 );
+}
+
+updateAxis(ctx);
